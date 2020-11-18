23a12ba3255d3127a6a10abf49d6dc0dff7190c8
Carlos <carlos.e.santaniello@gmail.com>
2020-11-18 13:00:56 +0100

    Implementaciones en ES7

---
+++ src/es7/index.js
@@ -0,0 +1,13 @@
+let numbers = [1, 2, 3, 7, 8];
+
+if (numbers.includes(7)){
+console.log('Number was founded');
+}else{
+    console.log('Number was not founded');
+}
+
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+
+console.log(`Base ^ exponent = ${result}`);
