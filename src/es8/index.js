793d7c2852cb9c34b86299783a25238d47f6ce61
Carlos <carlos.e.santaniello@gmail.com>
2020-11-18 13:43:21 +0100

    Async Await

--- src/es8/index.js
+++ src/es8/index.js
@@ -15,3 +15,30 @@ console.log(values.length);
 const string = 'caso';
 console.log(string.padStart(5,'o'));
 console.log(string.padEnd(6,'ta'));
+
+//Async Await
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+        ? setTimeout(() => resolve('Hello World'), 1000)
+        : reject(new Error('Test error'))
+    })
+};
+
+const helloAsync = async() => {
+    const hello = await helloWorld();
+    console.log(hello);
+}
+
+helloAsync();
+
+const anotherFunction = async() => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    }catch(error){
+        console.log(error);
+    }
+};
+
+anotherFunction();
